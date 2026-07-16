9aedd3b9e8b99b38ab8496ae78754f08ad892d99
Ilias Oluwapelumi <Oyedokunilias.com>
2026-07-16 13:38:49 +0100

    airtime

--- src/services/subandgain.service.js
+++ src/services/subandgain.service.js
@@ -1,4 +1,4 @@
-const axios = require('axios');
+const axios = require("axios");
 
 const api = axios.create({
     baseURL: process.env.SUBANDGAIN_BASE_URL,
@@ -8,43 +8,114 @@ const api = axios.create({
 const username = process.env.SUBANDGAIN_USERNAME;
 const apiKey = process.env.SUBANDGAIN_API_KEY;
 
-/**
- * Buy Airtime
- */
+/*
+|--------------------------------------------------------------------------
+| NETWORK MAPPING
+|--------------------------------------------------------------------------
+*/
+
+const NETWORKS = {
+    MTN: "MTN",
+    Airtel: "AIRTEL",
+    Glo: "GLO",
+    "9Mobile": "9MOBILE",
+};
+
+/*
+|--------------------------------------------------------------------------
+| Generic Request Function
+|--------------------------------------------------------------------------
+*/
+
+async function request(endpoint, params = {}) {
+    try {
+
+        console.log("==================================");
+        console.log("SUBANDGAIN REQUEST");
+        console.log(endpoint);
+        console.log(params);
+        console.log("==================================");
+
+        const { data } = await api.get(endpoint, {
+            params: {
+                username,
+                apiKey,
+                ...params,
+            },
+        });
+
+        console.log("==================================");
+        console.log("SUBANDGAIN RESPONSE");
+        console.log(data);
+        console.log("==================================");
+
+        // Provider returned an error
+        if (data.error) {
+            throw new Error(data.description || data.error);
+        }
+
+        return data;
+
+    } catch (err) {
+
+        if (err.response?.data) {
+
+            throw new Error(
+                err.response.data.description ||
+                err.response.data.error ||
+                "SubAndGain Error"
+            );
+
+        }
+
+        throw err;
+    }
+}
+
+/*
+|--------------------------------------------------------------------------
+| AIRTIME
+|--------------------------------------------------------------------------
+*/
+
 async function buyAirtime({
     network,
     phone,
     amount,
 }) {
-    try {
 
-        const response = await api.get('/airtime.php', {
-            params: {
-                username,
-                apiKey,
-                network,
-                phoneNumber: phone,
-                amount,
-            },
-        });
+    const apiNetwork = NETWORKS[network];
 
-        console.log(response.data);
-
-        return response.data;
-
-    } catch (err) {
-
-        if (err.response) {
-            throw new Error(
-                err.response.data?.message ||
-                'SubAndGain Airtime API Error'
-            );
-        }
-
-        throw new Error(err.message);
+    if (!apiNetwork) {
+        throw new Error("Unsupported Network");
     }
+
+    return request("/airtime.php", {
+        network: apiNetwork,
+        phoneNumber: phone,
+        amount,
+    });
+
+}
+
+/*
+|--------------------------------------------------------------------------
+| QUERY AIRTIME
+|--------------------------------------------------------------------------
+*/
+
+async function queryAirtime(trans_id) {
+
+    return request("/query_airtime.php", {
+        trans_id,
+    });
+
 }
 
 module.exports = {
+
     buyAirtime,
+
+    queryAirtime,
+
 };
